bf7564ce7ba0870e9d52bb4c0f6a1f4a633cb2b9
Hồng phong <hongphong0909.13@gmail.com>
2026-08-03 11:30:38 +0700

    feat: implement /status command

--- handlers/status.py
+++ handlers/status.py
@@ -1,17 +1,25 @@
 import telebot
-from config import ADMIN_ID, RUN_TIMEOUT
+from config import ADMIN_ID, RUN_TIMEOUT, LOG_FILE
+import platform
+from pathlib import Path
 
 
 def register_status(bot: telebot.TeleBot):
+    # Hàm lấy thông tin và tạo câu trả lời.
+    def get_info() -> str:
+        cwd = Path.cwd()
+        python_version = platform.python_version()
+        return (
+            "Agent Status\n\n"
+            f"Thư mục hiện tại: {cwd}\n"
+            f"Phiên bản python: {python_version}\n"
+            f"Thời gian timeout của file là: {RUN_TIMEOUT} giây\n"
+            f"Đường dẫn tới file log: {LOG_FILE}"
+        )
+
     # Hàm kiểm tra trạng thái chi tiết của bot.
     @bot.message_handler(commands=["status"])
     def status(message):
         if message.chat.id != ADMIN_ID:
             return
-        bot.reply_to(
-            message,
-            (
-                "🤖 bot đang hoạt động.\n"
-                f"File sẽ timeout sau: {RUN_TIMEOUT} giây"
-            ),
-        )
+        bot.reply_to(message, get_info())
